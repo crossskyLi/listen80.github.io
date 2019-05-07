be9b4251339b5fd63af0830911aa09c790486611
Li Jiabin <lijiabin1@domain.sensetime.com>
2019-05-07 15:27:59 +0800

    修正jsnesBUG

--- other/jsnes/index.js
+++ other/jsnes/index.js
@@ -1,4 +1,4 @@
-var speakers = new Speakers
+var speakers = new Speakers()
 var canvas = document.getElementById('canvas')
 var canvasContext = canvas.getContext('2d')
 var width = 256,
@@ -8,18 +8,8 @@ canvasContext.fillRect(0, 0, width, height)
 
 var imageData = canvasContext.getImageData(0, 0, 256, 240)
 
-function loading() {
-  cancelAnimationFrame(id)
-  canvasContext.fillStyle = 'black'
-  canvasContext.fillRect(0, 0, width, height)
-  canvasContext.font = "30px";
-  canvasContext.textAlign = 'center'
-  canvasContext.fillStyle = 'white'
-  canvasContext.fillText("正在载入", 128, 100)
-}
-
 var nes = new jsnes.NES({
-  onFrame: function(frameBuffer) {
+  onFrame: function (frameBuffer) {
     var pixel, i, j;
     var data = imageData.data;
     for (var i = 0; i < total; i++) {
@@ -31,22 +21,40 @@ var nes = new jsnes.NES({
     }
     canvasContext.putImageData(imageData, 0, 0);
   },
-  onAudioSample: function(left, right) {
+  onAudioSample: function (left, right) {
     speakers.writeSample(left, right)
   }
 });
 
-var xhr 
+
+var id
+
+function loading() {
+  cancelAnimationFrame(id)
+  canvasContext.fillStyle = 'black'
+  canvasContext.fillRect(0, 0, width, height)
+  canvasContext.font = "30px";
+  canvasContext.textAlign = 'center'
+  canvasContext.fillStyle = 'white'
+  canvasContext.fillText("正在载入", 128, 100)
+}
+
+function start() {
+  nes.frame()
+  id = requestAnimationFrame(frame)
+}
+
+var xhr
 function ajax(url, binary) {
 
   xhr && xhr.abort()
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     xhr = new XMLHttpRequest
     if (binary) {
       xhr.overrideMimeType('text/plain; charset=x-user-defined');
     }
 
-    xhr.onreadystatechange = function(e) {
+    xhr.onreadystatechange = function (e) {
       if (xhr.readyState == 4 && xhr.status == 200) {
         resolve(xhr.responseText)
       }
@@ -56,25 +64,26 @@ function ajax(url, binary) {
   })
 }
 
-var id
-
 ajax('roms.json').then((data) => {
   var roms = document.getElementById('roms')
-  var li = null
-  JSON.parse(data).roms.forEach(function(v, i) {
+  var pre_li = null
+  JSON.parse(data).roms.forEach(function (v, i) {
     var li = document.createElement('li')
-    li.innerHTML = v.replace('.nes', '');
+    li.innerText = v.replace('.nes', '');
     li.onclick = function () {
-      loading()
-      if(li) {
-        li.classList.remove('active')
+      if (li.classList.contains('active')) {
+        return
       }
-      li = this
+      loading()
+      if (pre_li) {
+        pre_li.classList.remove('active')
+      }
+      pre_li = li
       li.classList.add('active')
       ajax('roms/' + v, true).then((rom) => {
 
         nes.loadROM(rom)
-        frame()
+        start()
       })
     }
     !i && li.onclick()
@@ -83,57 +92,54 @@ ajax('roms.json').then((data) => {
   roms.style.display = 'block'
 })
 
-
-function frame() {
-  nes.frame()
-  id = requestAnimationFrame(frame)
-}
-
+// 按键
+var Controller = jsnes.Controller
 var keys = {
-  'w': jsnes.Controller.BUTTON_UP,
-  's': jsnes.Controller.BUTTON_DOWN,
-  'a': jsnes.Controller.BUTTON_LEFT,
-  'd': jsnes.Controller.BUTTON_RIGHT,
-  'j': jsnes.Controller.BUTTON_B,
-  'k': jsnes.Controller.BUTTON_A,
-  'Enter': jsnes.Controller.BUTTON_START,
-  'Shift': jsnes.Controller.BUTTON_SELECT,
+  'w': Controller.BUTTON_UP,
+  's': Controller.BUTTON_DOWN,
+  'a': Controller.BUTTON_LEFT,
+  'd': Controller.BUTTON_RIGHT,
+  'j': Controller.BUTTON_B,
+  'k': Controller.BUTTON_A,
+  'Enter': Controller.BUTTON_START,
+  'Shift': Controller.BUTTON_SELECT,
 }
 
 var keys2 = {
-  'ArrowUp': jsnes.Controller.BUTTON_UP,
-  'ArrowDown': jsnes.Controller.BUTTON_DOWN,
-  'ArrowLeft': jsnes.Controller.BUTTON_LEFT,
-  'ArrowRight': jsnes.Controller.BUTTON_RIGHT,
-  '1': jsnes.Controller.BUTTON_B,
-  '2': jsnes.Controller.BUTTON_A,
-  // 'Enter': jsnes.Controller.BUTTON_START,
-  // 'Shift': jsnes.Controller.BUTTON_SELECT,
+  'ArrowUp': Controller.BUTTON_UP,
+  'ArrowDown': Controller.BUTTON_DOWN,
+  'ArrowLeft': Controller.BUTTON_LEFT,
+  'ArrowRight': Controller.BUTTON_RIGHT,
+  '1': Controller.BUTTON_B,
+  '2': Controller.BUTTON_A,
+  // 'Enter': Controller.BUTTON_START,
+  // 'Shift': Controller.BUTTON_SELECT,
 }
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
   nes.buttonDown(1, keys[e.key]);
   nes.buttonDown(2, keys2[e.key]);
 })
 
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
   nes.buttonUp(1, keys[e.key]);
   nes.buttonUp(2, keys2[e.key]);
 })
 
-document.addEventListener('drop', function(e) {
+// 拖拽文件
+document.addEventListener('drop', function (e) {
   e.preventDefault()
 
   loading()
 
   var reader = new FileReader();
   reader.readAsBinaryString(e.dataTransfer.files[0])
-  reader.onload = function() {
+  reader.onload = function () {
     nes.loadROM(this.result)
     frame()
   }
 })
 
-document.addEventListener('dragover', function(e) {
+document.addEventListener('dragover', function (e) {
   e.preventDefault()
 })
